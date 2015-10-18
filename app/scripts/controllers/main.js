6e472cb83915944e8dbcb5a6c3f75c417e2c19ec
Giulio Seregni <giulio.seregni@pingendo.com>
2015-10-18 19:22:36 +0200

    more on geo fire

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,60 +8,70 @@
  * Controller of the geosApp
  */
 angular.module('geosApp')
-  .controller('MainCtrl', function ($scope, Ref, $geofire) {
+  .controller('MainCtrl', function ($scope, $geofire, uiGmapGoogleMapApi) {
     
-  	$scope.map = { center: { latitude: 45, longitude: -73 }, zoom: 8 };
+  	$scope.map = { 
+		center: { latitude: 45, longitude: -73 }, 
+		zoom: 8,
+		events: {
+            tilesloaded: function (map) {
+            	var center  = map.getCenter();
+            	console.log(center.G," - ", center.K);
+            	query.updateCriteria({
+				  center: [center.G, center.K],
+				  radius: 5
+				});
+                // $scope.$apply(function () {
+                // 	console.log("map ", map.getCenter());
+                //     $scope.mapInstance = map;
 
-    // var cards = $firebaseObject(Ref.child('thecareworld/cards'));
-    // console.log("cards ", cards);
-    // cards.$bindTo($scope, 'cards');
-
-
+                // });
+            }
+        }
+  	};
 
      $scope.searchResults = [];
 
-    var $geo = $geofire(new Firebase('https://skeleton-firebase.firebaseio.com/thecareworld/cards'));
+    var $geo = $geofire(new Firebase('https://skeleton-firebase.firebaseio.com/thecareworld/geos'));
 
     // Trivial example of inserting some data and querying data
-    // $geo.$set("some_key", [37.771393,-122.447104])
-    //     .catch(function(err) {
-    //         $log.error(err);
-    //     });
-
-    // // Get some_key's location, place it on $scope
-    // $geo.$get("some_key")
-    //     .then(function (location) {
-    //         $scope.objLocation = location;
-    //     });
-
-    // // Remove "some_key" location from forge
-    // $geo.$remove("some_key")
-    //     .catch(function (err) {
-    //         $log.error(err);
-    //     });
-
+    $geo.$set("tre", [45,-73])
+        .catch(function(err) {
+            $log.error(err);
+        });
 
     // Setup a GeoQuery
     var query = $geo.$query({
-        center: [37.77, -122.447],
+        center: [45, -73],
         radius: 2000
-    }, function(err,data) {
-    	alert(data);
     });
 
     // // Setup Angular Broadcast event for when an object enters our query
     var geoQueryCallback = query.on("key_entered", "SEARCH:KEY_ENTERED");
+    var geoQueryCallback = query.on("key_exited", "SEARCH:KEY_EXITED");
 
     // // Listen for Angular Broadcast
     $scope.$on("SEARCH:KEY_ENTERED", function (event, key, location, distance) {
-    	alert("jkj");
         // Do something interesting with object
+        console.log("key entered", key);
         $scope.searchResults.push({key: key, location: location, distance: distance});
 
         // Cancel the query if the distance is > 5 km
         if(distance > 5) {
             geoQueryCallback.cancel();
         }
+    });
+
+
+    $scope.$on("SEARCH:KEY_EXITED", function (event, key, location, distance) {
+        // Do something interesting with object
+        console.log("key exited", key);
+        // $scope.searchResults.push({key: key, location: location, distance: distance});
+
+        // Cancel the query if the distance is > 5 km
+        // if(distance > 5) {
+        //     geoQueryCallback.cancel();
+        // }
     });
 
 
