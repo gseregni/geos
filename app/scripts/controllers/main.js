043a53c03f7bffba8ef1a1e053e9e055a203f3ba
Giulio Seregni <giulio.seregni@pingendo.com>
2015-10-21 16:07:14 +0200

    highlight marker on over card

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -64,43 +64,30 @@ angular.module('geosApp')
                     var geocoder = new google.maps.Geocoder();
                     geocoder.geocode({'address': $routeParams.location}, function(results, status) {
                         if (status === google.maps.GeocoderStatus.OK) {
-                            // if (!mapInstance)
-                            //     mapInstance = results[0].geometry.location;
-                            // else {
 
-                                mapInstance.setCenter(results[0].geometry.location);
-                                mapInstance.fitBounds(results[0].geometry.viewport);
-                                mapChanged();
+                            mapInstance.setCenter(results[0].geometry.location);
+                            mapInstance.fitBounds(results[0].geometry.viewport);
+                            mapChanged();
 
-                                console.log("viewport " , results[0].geometry.viewport.getNorthEast().lat());
-                                var ne = results[0].geometry.viewport.getNorthEast();
-                                 // r = radius of the earth in statute miles
-                                var r = 3963.0;  
+                            var ne = results[0].geometry.viewport.getNorthEast();
+                            var r = 3963.0;
 
-                                // Convert lat or lng from decimal degrees into radians (divide by 57.2958)
-                                var lat1 = mapInstance.center.lat() / 57.2958; 
-                                var lon1 = mapInstance.center.lng() / 57.2958;
-                                var lat2 = ne.lat() / 57.2958;
-                                var lon2 = ne.lng() / 57.2958;
+                            // Convert lat or lng from decimal degrees into radians (divide by 57.2958)
+                            var lat1 = mapInstance.center.lat() / 57.2958;
+                            var lon1 = mapInstance.center.lng() / 57.2958;
+                            var lat2 = ne.lat() / 57.2958;
+                            var lon2 = ne.lng() / 57.2958;
 
-                                // // distance = circle radius from center to Northeast corner of bounds
-                                var dis = r * Math.acos(Math.sin(lat1) * Math.sin(lat2) + 
-                                  Math.cos(lat1) * Math.cos(lat2) * Math.cos(lon2 - lon1));
+                            var dis = r * Math.acos(Math.sin(lat1) * Math.sin(lat2) + 
+                              Math.cos(lat1) * Math.cos(lat2) * Math.cos(lon2 - lon1));
 
-                                setupGeo([mapInstance.center.lat(), mapInstance.center.lng()], dis);
-                                // map.setCenter(mapInstance);
-                            // }
+                            setupGeo([mapInstance.center.lat(), mapInstance.center.lng()], dis);
 
                         } else {
                           alert('Geocode was not successful.' + status);
                         }
                       });
                 }
-                
-
-                    
-                    
-            	
             },
             zoom_changed : mapChanged,
             dragend : mapChanged
@@ -111,6 +98,13 @@ angular.module('geosApp')
         window.location.hash = "#/search/" + $scope.search;
     }
 
+    $scope.highlightMarker = function(card) {
+        card.options = { animation: 1 };
+    }
+
+    $scope.unhighlightMarker = function(card) {
+        card.options = { animation: 0 };
+    }
     
 
     $scope.searchResults = [];
@@ -130,7 +124,6 @@ angular.module('geosApp')
             var fredRef = Ref.child('thecareworld/cards/' + key);
             console.log("query", 'thecareworld/cards/' + key);
             fredRef.once("value", function(snapshot) {
-                    console.log("RESULT ",  snapshot);
                 
                 var m = snapshot.val();
 
